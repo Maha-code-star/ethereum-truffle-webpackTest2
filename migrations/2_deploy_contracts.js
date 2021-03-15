f24da94d609a9ac1611f1962988cc545363390a3
Maha-code-star <74078213+Maha-code-star@users.noreply.github.com>
2021-03-15 11:40:41 +0300

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,8 @@
-const ConvertLib = artifacts.require("Insurance");
-
+const ConvertLib = artifacts.require("ConvertLib");
+const MetaCoin = artifacts.require("MetaCoin");
 
 module.exports = function(deployer) {
-
-  deployer.deploy(Insurance);
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
 };
