6c253bf80bef5d99cce9e663cb17925855be0c2c
Maha-code-star <74078213+Maha-code-star@users.noreply.github.com>
2021-03-19 14:08:34 +0300

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const Insurance = artifacts.require("Insurance");
+
+contract("Insurance", async function(accounts){
+
+it (
+    "should add a product", async function(){
+        //let Product ID = 1;
+        let instance = await Insurance.deployed()
+        await instance.addProduct(1,"iphone",web3.utils.toWei('7', 'ether')) });
+
+}
